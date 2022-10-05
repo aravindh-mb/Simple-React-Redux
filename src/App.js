c138c5e8a4462cc87cf3b8aea2437fb0e77a82cc
Aravindh <aaravindh23cse@gmail.com>
2022-10-06 00:51:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {vadivelucomedy,goundamanicomedy} from './index.js';
 
-function App() {
+const App = () => {
+  const comedies = useSelector(state =>state)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+       <p>{comedies}</p>
+       <button onClick={()=> dispatch(vadivelucomedy())}>vadivelu comedy</button>
+       <button onClick={()=> dispatch(goundamanicomedy())}>goundamani comedy</button>
+       
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,14 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+
+      // selector
+      const VADIVELU_COMEDY = "VADIVELU_COMEDY";
+      const GOUNDAMANI_COMEDY ="GOUNDAMANI_COMEDY";
+
+      // Action
+      export const vadivelucomedy = ()=> ({type:VADIVELU_COMEDY})
+      export const goundamanicomedy =()=>({type:GOUNDAMANI_COMEDY})
+
+      // reducer 
+      const comedyReducer =(state,actions) =>{
+          switch(actions.type){
+              case VADIVELU_COMEDY:
+                  return state ="snake babu";
+              case GOUNDAMANI_COMEDY:
+                  return state ="arasiyalla ithellam satharnamappa";
+              default:
+                return state = "No comedy selected";
+          }
+      }
+      
+let store = createStore(comedyReducer);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+          <App  />
+    </Provider>
   </React.StrictMode>
 );
-
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
