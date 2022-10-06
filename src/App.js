d92f5937989dc102f77a89a887416a0fada6945e
Aravindh <aaravindh23cse@gmail.com>
2022-10-06 22:11:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {vadivelucomedy,goundamanicomedy} from './index.js';
+import {vadivelucomedy,goundamanicomedy,isSubscribed} from './index.js';
 
 const App = () => {
   const comedies = useSelector(state =>state)
   const dispatch = useDispatch()
   return (
     <React.Fragment>
-       <p>{comedies}</p>
-       <button onClick={()=> dispatch(vadivelucomedy())}>vadivelu comedy</button>
-       <button onClick={()=> dispatch(goundamanicomedy())}>goundamani comedy</button>
-       
+      <p>{comedies.isSubscribed?'true':'false'}</p>
+       <img  src={comedies.img} alt={"img"} />
+       <button disabled={!comedies.isSubscribed} onClick={()=> dispatch(vadivelucomedy())}>vadivelu comedy</button>
+       <button disabled={!comedies.isSubscribed} onClick={()=> dispatch(goundamanicomedy())}>goundamani comedy</button>
+       <button disabled={!comedies.isSubscribed} onClick={dispatch(isSubscribed())}>{comedies.isSubscribed ? "Subscribe":"Unsubscribe"}</button>
+
     </React.Fragment>
   )
 }
 
-export default App
+export default App 
